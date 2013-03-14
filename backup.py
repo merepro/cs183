c4f3b065c62cf638e66822ae5535c344a63f1491
Kevin <kevin@ubuntu.(none)>
2013-03-14 00:09:28 -0700

    Changed backup.py

--- backup.py
+++ backup.py
@@ -5,13 +5,13 @@ from time import gmtime, strftime
 stamp = time.strftime("%Y%m%d%H%M%S", gmtime())
 
 
-#shutil.copy2(sys.argv[1], sys.argv[1]+'.'+stamp)
+shutil.copy2(sys.argv[1], sys.argv[1]+'.'+stamp)
 
-#diff=filecmp.cmp(sys.argv[1],sys.argv[1]+'.'+stamp)
+diff=filecmp.cmp(sys.argv[1],sys.argv[1]+'.'+stamp)
 
-shutil.copy2(sys.argv[1], '/home/kevin/Desktop/newf.'+stamp)
+#shutil.copy2(sys.argv[1], '/home/kevin/Desktop/newf.'+stamp)
 
-diff=filecmp.cmp(sys.argv[1],'/home/kevin/Desktop/newf.'+stamp)
+#diff=filecmp.cmp(sys.argv[1],'/home/kevin/Desktop/newf.'+stamp)
 if diff ==True:
 	pass
 	print sys.argv[1]+" has been backed up."
